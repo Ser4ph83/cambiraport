0f1c89d276d81ef7f92a778410a7a0c83cf1bbfe
ser4ph83 <gilson.terada@docente.pr.senac.br>
2024-11-06 15:17:21 -0300

    Add files via upload

---
+++ scriptport.js
@@ -0,0 +1,14 @@
+document.getElementById('form-contato').addEventListener('submit', function(e) {
+  e.preventDefault();
+  const nome = document.getElementById('nome').value;
+  const email = document.getElementById('email').value;
+  const mensagem = document.getElementById('mensagem').value;
+
+  if (nome && email && mensagem) {
+    alert('Mensagem enviada com sucesso!');
+    
+    document.getElementById('form-contato').reset();
+  } else {
+    alert('Por favor, preencha todos os campos.');
+  }
+});
